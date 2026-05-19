b2c39d0347d0c25751e7ea0daf75bfe29ac7252b
zehragulbuyukarslan <zgbuyukarslan@icloud.com>
2026-05-19 19:00:55 +0300

    update auth background

--- register.php
+++ register.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>İHA Sistem - Kayıt</title>
+    <title>TULPAR - Kayıt</title>
     <link rel="stylesheet" href="assets/css/auth.css">
 </head>
 <body class="auth-page">
